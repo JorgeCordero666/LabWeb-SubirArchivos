782d2ce35b5551e3aa3bd1d198fe8e0e9ac71ba0
JorgeCordero666 <jorge666.cordero@gmail.com>
2023-08-16 17:33:37 -0500

    Subir Archivos Version 3

--- app.js
+++ app.js
@@ -1,44 +1,30 @@
 const fileInput = document.getElementById('fileInput');
-
 const fileInfo = document.getElementById('fileInfo');
-
 const preview = document.getElementById('preview');
 
-
 fileInput.addEventListener('change', function(event) {
-
   const selectedFile = event.target.files[0];
 
-
-  fileInfo.innerHTML = `
-
-    Nombre del archivo: ${selectedFile.name}<br>
-
-    Tipo MIME: ${selectedFile.type}<br>
-
-    Tamaño: ${selectedFile.size} bytes
-
-  `;
-
-
-  if (selectedFile.type.startsWith('image/')) {
-
-    preview.style.display = 'block';
-
-    const reader = new FileReader();
-
-    reader.onload = function(e) {
-
-      preview.src = e.target.result;
-
-    };
-
-    reader.readAsDataURL(selectedFile);
-
-  } else {
-
+  if (selectedFile.size > 7 * 1024 * 1024) {
+    fileInfo.innerHTML = `El archivo excede el límite de 7 MB.`;
     preview.style.display = 'none';
-
+    return;
   }
 
+  fileInfo.innerHTML = `
+    Nombre del archivo: ${selectedFile.name}<br>
+    Tipo MIME: ${selectedFile.type}<br>
+    Tamaño: ${selectedFile.size} bytes
+  `;
+
+  if (selectedFile.type.startsWith('image/')) {
+    preview.style.display = 'block';
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      preview.src = e.target.result;
+    };
+    reader.readAsDataURL(selectedFile);
+  } else {
+    preview.style.display = 'none';
+  }
 });
