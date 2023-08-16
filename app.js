53bbfeb2d1ad462ad772db806186b99c4c9c9b7a
AngeloSFV2001 <angelo.fabara@epn.edu.ec>
2023-08-16 16:55:40 -0500

    Subir Archivos Version 1

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const fileUpload = require('express-fileupload')
+
+
+const app = express()
+
+
+app.use(fileUpload())
+
+app.post('/upload',(req,res) => {
+
+    let EDFile = req.files.file
+
+    EDFile.mv(`./files/${EDFile.name}`,err => {
+
+        if(err) return res.status(500).send({ message : err })
+
+
+        return res.status(200).send({ message : 'Archivo subido' })
+
+    })
+
+})
+
+app.listen(3000,() => console.log('Corriendo'))
